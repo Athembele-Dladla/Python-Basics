8f0edabde3dff1a262eabdecd00ffb29d47dae8f
Athembele A. Dladla <dladlaathembele@gmail.com>
2026-03-15 19:30:17 +0200

    strings2.0

---
+++ strings2.py
@@ -0,0 +1,11 @@
+#advanced concepts - strings
+
+message = "hello, world"
+
+#print(message[0])
+#print(message[1])
+
+print(message.strip())#remove leading and trailing white space
+print(message.lower())#convert all characters to lowercase
+print(message.split())#plit string into a list based on comma
+print(len(message))
