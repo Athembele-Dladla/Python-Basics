b686aa3222c9686c5430e8fcef60905a6ed66cb9
Athembele A. Dladla <dladlaathembele@gmail.com>
2026-03-15 19:28:44 +0200

    operators

---
+++ operators.py
@@ -0,0 +1,21 @@
+#addition(+)
+#subtraction(-)
+#muiltiplication(*)
+#division(/)
+#modulus(%)reminder after dividing
+#exponent(**)
+
+#x = 10
+#y = 2
+
+#print(x+y)
+#print(x-y)
+#print(x/y)
+#print(x**y)
+#print(x*y)
+
+x = 10
+#x = x + 2
+x +=2
+
+print(x)
